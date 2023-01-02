d0ae7b028c358b22136c0230bb700702de0b8009
jmargrove <grovejam@gmail.com>
2023-01-02 15:45:08 +0100

    Updating to generic - files and dirs, and dry run default

--- index.sh
+++ index.sh
@@ -1,8 +1,15 @@
 #!/bin/bash 
+flag_w=false
+flag_f=false
+flag_d=false
 
 # Parse command-line options
-while getopts ":d" opt; do
+while getopts "fdw" opt; do
   case $opt in
+    w)
+      # Flag for a wet run - default is for dry
+      flag_w=true
+      ;;
     f)
       # Flag -f is present
       flag_f=true
@@ -16,24 +23,35 @@ while getopts ":d" opt; do
       echo "Invalid option: $OPTARG" >&2
       exit 1
       ;;
-    :)
-      # Option requires an argument
-      echo "Optioin -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
   esac
 done
 
-sed_cmd='file=$((echo $1 | gsed -r "s/(\/)(.)/\1\U\2/g" | gsed -r "s/(^|-|_|\s)([a-z0-9])/\U\2/g" | gsed -r "s/\s//g") | tr -d "\n")
-  mv "$1" "$file"
-'
+
+# sed_cmd='file=$((echo $1 | gsed -r "s/(\/)(.)/\1\U\2/g" | gsed -r "s/(^|-|_|\s)([a-z0-9])/\U\2/g" | gsed -r "s/\s//g") | tr -d "\n") \ echo "$1" "$file"'
+
+wet_sed_cmd='file=$((echo $1 | gsed -r "s/(\/)(.)/\1\U\2/g" | gsed -r "s/(^|-|_|\s)([a-z0-9])/\U\2/g" | gsed -r "s/\s//g") | tr -d "\n")
+mv "$1" "$file"'
+
+dry_sed_cmd='file=$((echo $1 | gsed -r "s/(\/)(.)/\1\U\2/g" | gsed -r "s/(^|-|_|\s)([a-z0-9])/\U\2/g" | gsed -r "s/\s//g") | tr -d "\n")
+echo "$1" "$file"'
+
 # Rename all files and directories under the current directory to PascalCase
-if [ "$flag_d"  = true ]; then
+if [[ "$flag_w" = true && "$flag_d" = true ]]; then
   # Run the script on the files
-  find . -type f -exec sh -c $sed_cmd sh {} \;
-if else [ "$flag_f" = true ]; then
+  echo "Dirs renaming..."
+  find . -type f -exec sh -c "$wet_sed_cmd" sh {} \;
+elif [[ "$flag_w" = true && "$flag_f" = true ]]; then
   # Run the script on the directories
-  find . -type d -exec sh -c $sed_cmd sh {} \;
+  echo "Files renaming..."
+  find . -type f -exec sh -c "$wet_sed_cmd" sh {} \;
+elif [[ "$flag_w" = false && "$flag_f" = true ]]; then
+  # Run the script on the directories
+  echo "Files testing..."
+  find . -type f -exec sh -c "$dry_sed_cmd" sh {} \;
+elif [[ "$flag_w" = false && "$flag_d" = true ]]; then
+  # Run the script on the directories
+  echo "Dirs testing..."
+  find . -type d -exec sh -c "$dry_sed_cmd" sh {} \;
 else 
   # Flag is not present
   echo "Flag is not present."
